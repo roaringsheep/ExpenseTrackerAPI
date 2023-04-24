b4a64c02da9e20cb6d5c199d88929f3be0f0e0be
Minseon Song <songminseonk@gmail.com>
2023-04-24 18:07:25 -0400

    dists

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,5 +5,15 @@ namespace ExpenseTrackerAPI.Controllers;
 [Route("api/[controller]")]
 public class UserController : ControllerBase
 {
-    
+    [HttpGet]
+    public string Get(string? name) {
+        if(name != null)
+        {
+            return $"Hello {name}!";
+        }
+        else 
+        {
+            return "Hello World!";
+        }
+    }
 }
